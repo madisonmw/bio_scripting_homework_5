75339bf72b45bed1c3779501adcd769aa9cba996
Madison M. Watkins <mmw0038@auburn.edu>
2020-02-21 18:19:06 -0600

    Added function that makes input (numbers) into integers

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -17,6 +17,9 @@ print("You entered", b, "which is a", type(b))
 #       Let's assume we want a and b to be integers at this point
 #######################################################################
 
+a = int(a) #Makes input from a and b into integers. At the moment only works with actual integers.
+b = int(b)
+
 print("What is", a, "now?", type(a))
 print("What is", b, "now?", type(a))
 
