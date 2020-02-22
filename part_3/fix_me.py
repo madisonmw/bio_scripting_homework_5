4134072ea78bbea37b720f0a162aea2fc29c36d5
Madison M. Watkins <mmw0038@auburn.edu>
2020-02-22 01:02:16 -0600

    Added more specificity for input and more while/try loops

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -3,23 +3,25 @@
 #Changed shebang line to be correct. Was originally calling the shell.
 #Reading data from the user's input
 
-print("Hello, please enter a number.\n")
+print("Hello, please enter a number or press CTRL+C to quit.\n")
 
 #Added while loop with try/except added in. Although we really only need integers, I wanted to
 #find out a way to make floats work, too. eval() evaluates what type of input is
 #put in, so 2 is an int and 2.3 is a float, for example. While True is set, if type(a) is either a float
-#or int, the loop stops. Any other input causes a NameError exception that prompts you for
-#a number and continues the loop. I had to add the KeyboardInterrupt exception, since I could not ctrlC out of the
-#script without it. I am not sure what raise does; however, the exception does not work
-#without it.
+#or int, the loop stops. Other classes (that I know of) cause the loop to repeat until a number is put in. Input that is not
+#a class causes a NameError exception that prompts you for a number and continues the loop. The classes are checked
+#with the handy command isinstance. I had to add the KeyboardInterrupt exception, since I could not ctrlC
+#out of the script without it. I am not sure what raise does; however, the exception will not work without it.
 
 while True:
     try:
         a = eval(input("Enter a: "))
-        if type(a) == int or float:
+        if isinstance(a, (int, float)):
             break
+        elif isinstance(a, (str, tuple, list, complex)):
+            print("Please enter a number or press CTRL+C to quit.")
     except NameError:
-        print("Please enter a number.")
+        print("Please enter a number or press CTRL+C to quit.")
     except KeyboardInterrupt:
         raise
 
@@ -28,10 +30,12 @@ print("You entered", a, "which is a", type(a))
 while True:
     try:
         b = eval(input("Enter b: "))
-        if type(b) == int or float:
+        if isinstance(b, (int, float)):
             break
+        elif isinstance(b, (str, tuple, list, complex)):
+            print("Please enter a number of press CTRL+C to quit.")
     except NameError:
-        print("Please enter a number.")
+        print("Please enter a number or press CTRL+C to quit.")
     except KeyboardInterrupt:
         raise
 
@@ -56,18 +60,38 @@ print("a - b =", difference, ", which is a", type(difference))
 product = a * b
 print("a * b =", product, ", which is a", type(product))
 
-quotient = a / b
-print("a / b =", quotient, ", which is a", type(quotient))
+#Added while loops for all division equations. This allows me to address
+#a situation when someone may divide by zero. A break is required for both the
+#try and except blocks, otherwise you get stuck in a never-ending loop.
 
-floor_quotient = a // b
-print("a // b =",
-        floor_quotient,
-        ", which is a",
+while True:
+    try:
+        quotient = a / b
+        print("a / b =", quotient, ", which is a", type(quotient))
+        break
+    except ZeroDivisionError:
+        print("a / b = ... Nothing! You can't divide by zero!")
+        break
+
+while True:
+    try:
+        floor_quotient = a // b
+        print("a // b =", floor_quotient,", which is a",
         type(floor_quotient),
         )
+        break
+    except ZeroDivisionError:
+        print("a // b = ... Nothing! You can't divide by zero!")
+        break
 
-remainder = a % b
-print("a % b =", remainder, ", which is a", type(remainder))
+while True:
+    try:
+        remainder = a % b
+        print("a % b =", remainder, ", which is a", type(remainder))
+        break
+    except ZeroDivisionError:
+        print("a % b = ... Nothing! You can't divide by zero!")
+        break
 
 power = a ** b
 print("a ** b =", power, ", which is a", type(power))
